6d490fdf0e3c5e3a5c4c66acfcc5add49a3432ac
randisunarto <148059279+randisunarto@users.noreply.github.com>
2023-12-10 13:47:59 +0700

    fungsi untuk memilih matakuliah dan data kelas

--- Digital-Attendance.c
+++ Digital-Attendance.c
@@ -5,7 +5,7 @@
 
 #define MAX_MURID 100
 #define MAX_NIM 10000
-#define MAX_KELAS 5
+#define MAX_KELAS 9
 
 struct Mahasiswa {
     char nama[MAX_MURID][50];
@@ -24,8 +24,12 @@ struct nimNamaData {
 struct Dosen {
     char nama[50];
 };
+int main() {
+    struct Mahasiswa Mahasiswas;
+    int nmMahasiswas = 0;
+    int totalMahasiswa = 29;
 
-struct  dosen[100]{
+struct Dosen[MAX_KELAS] = {
         {"Liptia Venica. S.T., M.T."},
         {"Dewi Indri Hadi Putri, S.Pd., M.T."},
         {"Suprih Widodo, S.Si., M.T."},
@@ -36,8 +40,9 @@ struct  dosen[100]{
         {"Hisny Fajjrussalam, M.Pd"},
         {"Diky Zakaria, S.Pd. M.T."},
     };
+    int Dosen = sizeof(Dosen) / sizeof(Dosen[0]);
 
-struct nim, nama[MAX_MURID][50]{
+struct nimNama nimNama[MAX_NIM] = {
         {2312388, "Farrel Shidqi Lazuardi"},
         {2312601, "Andre Saputra"},
         {2310303, "Ilmi Nurwahidah"},
@@ -45,8 +50,31 @@ struct nim, nama[MAX_MURID][50]{
         {2312169, "Randi Sunarto"},
         {2309650, "Alfarizi Khoerul Rizal"},
     };
+    int nimNama = sizeof(nimNama) / sizeof(nimNama[0]);
+char pilihMataKuliah[50];
 
-char mataKuliah[MAX_KELAS][50] = {
+    printf("Silahkan Mengisi Absensi!\n");
+    printf("\nPilih mata kuliah hari ini! (Pilih Menu Matakuliah, 0 untuk keluar):\n");
+    printf("1. Pemrograman Dasar \n");
+    printf("2. Elektronika Dasar\n");
+    printf("3. Matematika Dasar\n");
+    printf("4. Bahasa Inggris\n");
+    printf("5. Dasar Listrik\n");
+    printf("6. Fisika Teknik\n");
+    printf("7. Teknik Digital\n");
+    printf("8. Pendidikan Agama Islam\n");
+    printf("9. Pengantar TIK dan literasi TIK\n");
+     int Pilih;
+    scanf("%d", &Pilih);
+
+    if (Pilih == 0) {
+        return 0;
+    } else if (Pilih < 1 || Pilih > MAX_KELAS) {
+        printf("Pilihan tidak sesuai. Silahkan Pilih Menu Yang Ada.\n");
+        return 0;
+    }
+
+  char mataKuliah[MAX_KELAS][50] = {
         "Pemrograman Dasar",
         "Dasar Listrik",
         "Teknik Digital",
@@ -56,4 +84,5 @@ char mataKuliah[MAX_KELAS][50] = {
         "Elektronika Dasar",
         "Fisika Teknik",
         "Pendidikan Agama Islam",
-};
+  };
+    strcpy(pilihMataKuliah, mataKuliah[Pilih - 1]);
