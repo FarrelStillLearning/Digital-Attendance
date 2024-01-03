383aba5085cdf87b24e5ecdeb1905a185d0855d1
haqqyarifin <148345663+haqqyarifin@users.noreply.github.com>
2024-01-03 14:09:54 +0700

    Update Digital-Attendance.c

--- Digital-Attendance.c
+++ Digital-Attendance.c
@@ -3,9 +3,21 @@
 #include <string.h>
 #include <time.h>
 
-#define MAX_MURID 100
+#define MAX_MURID 1000
 #define MAX_NIM 10000
 #define MAX_KELAS 20
+
+struct WaktuAbsensi {
+    int jamMulai;
+    int menitMulai;
+    int jamSelesai;
+    int menitSelesai;
+};
+
+struct Dosen {
+    char nama[50];
+    struct WaktuAbsensi waktuAbsensi;
+};
 
 struct Mahasiswa {
     char nama[MAX_MURID][50];
@@ -13,7 +25,7 @@ struct Mahasiswa {
     char mataKuliah[MAX_MURID][50];
     char waktuHadir[MAX_MURID][30];
     char tanggal[MAX_MURID][50];
-    int dataKehadiran[MAX_MURID];
+    time_t dataKehadiran[MAX_MURID];
     char dosen[MAX_KELAS][50];
 };
 
@@ -22,16 +34,14 @@ struct dataNimNama {
     char nama[50];
 };
 
-struct Dosen {
-    char nama[50];
-};
-
 int pendataanKehadiran(struct Mahasiswa *mhsw, int *nomMhsw, const char *mataKuliah,
-                        struct dataNimNama nimNamaMap[], int *nomNimNamaMap, struct Dosen dosenMap[], int *nomDosenMap,
-                        int totalMahasiswa, int Pilih);
+                       struct dataNimNama nimNamaMap[], int *nomNimNamaMap, struct Dosen dosenMap[], int *nomDosenMap,
+                       int totalMahasiswa, int Pilih);
 
 int tampilkanData(struct Mahasiswa *mhsw, int nomMhsw, const char *pilihMataKuliah, int totalMahasiswa, int Pilih,
-                    struct Dosen dosenMap[]);
+                  struct Dosen dosenMap[]);
+
+int MataKuliah(char *pilihMataKuliah, int *Pilih);
 
 int main() {
     struct Mahasiswa mhsw;
